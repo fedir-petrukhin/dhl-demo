2032f4fe638d081a64fd961d5837d79b73219ee2
fedir-petrukhin <fpetrukhin@gmail.com>
2016-11-02 16:10:59 +0200

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,5 +1,5 @@
 angular.module('starter.services', ['ngResource'])
 
 .factory('Complaints', function ($resource) {
-    return $resource('http://complaints.cc-interactive.com.ua/imgmt/incidents');
+    return $resource('http://cci:a1s2d3f4@complaints.cc-interactive.com.ua/imgmt/incidents');
 });
